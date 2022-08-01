f382255ad5852716d9c110800bb7bd0d624999db
jjguerreros <jonelguerrero@gmail.com>
2022-08-01 17:08:56 +0200

    estructura pryecto

---
+++ vistas/inicio.php
@@ -0,0 +1,11 @@
+<?php include '../plantillas/cabecera.php'; ?>
+<div class="container-fluid">
+
+    <div class="text-center">
+        <h1>TOMA TU PATATA</h1>
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a5b478ff-e9de-43c5-9ecf-8e018e9ed56f/d816sz9-18ebb269-bbd3-443c-8626-7f016ad2f5f2.png/v1/fill/w_600,h_450,q_80,strp/the_crazy_potatoes_guitarist_by_oddiss_fan_d816sz9-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E1YjQ3OGZmLWU5ZGUtNDNjNS05ZWNmLThlMDE4ZTllZDU2ZlwvZDgxNnN6OS0xOGViYjI2OS1iYmQzLTQ0M2MtODYyNi03ZjAxNmFkMmY1ZjIucG5nIiwiaGVpZ2h0IjoiPD00NTAiLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd21cL2E1YjQ3OGZmLWU5ZGUtNDNjNS05ZWNmLThlMDE4ZTllZDU2Zlwvb2RkaXNzLWZhbi00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.AFnOGLx6jGdQ2dWMqKpbFfUIti7tAowKvfwSzJWmhUY">
+    </div>
+    <p class="text-end pe-4">¿Dónde la armamos?</p>
+
+</div>
+<?php include '../plantillas/pie.php'; ?>
